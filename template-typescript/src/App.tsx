b406179764a924671d6f75304104d778aa9f8a70
顾杰义 <18535@etransfar.com>
2020-03-16 14:44:40 +0800

    1

--- template-typescript/src/App.tsx
+++ template-typescript/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        hellow world!
+         hello world gujy
         </p>
       </header>
     </div>
